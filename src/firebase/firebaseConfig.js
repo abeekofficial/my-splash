1eca752fcdfadb1b8309183e04800cb76529ee42
Abbosbek Ruzimov <abeekoffi@gmail.com>
2025-04-05 23:10:32 +0500

    firebase installed

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA5oShsJfl8Ox6E5-ID1TQ3foZdScS6MyI",
+  authDomain: "mysplash-abeek.firebaseapp.com",
+  projectId: "mysplash-abeek",
+  storageBucket: "mysplash-abeek.firebasestorage.app",
+  messagingSenderId: "486893956334",
+  appId: "1:486893956334:web:438343dbf10174d64b57ff",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
