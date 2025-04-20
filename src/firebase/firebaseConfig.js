ae0a6222fa28ffacd6b8e68890bb9381352666e1
Abbosbek Ruzimov <abeekoffi@gmail.com>
2025-04-20 15:53:53 +0300

    Updated: registerWithEmail created

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,16 +1,17 @@
-import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA5oShsJfl8Ox6E5-ID1TQ3foZdScS6MyI",
-  authDomain: "mysplash-abeek.firebaseapp.com",
-  projectId: "mysplash-abeek",
-  storageBucket: "mysplash-abeek.firebasestorage.app",
-  messagingSenderId: "486893956334",
-  appId: "1:486893956334:web:438343dbf10174d64b57ff",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth();
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
